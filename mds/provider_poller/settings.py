3847c72ce2d59bd74e6d60dfd1e36898d5b8e234
Hervé Cauwelier <herve.cauwelier@polyconseil.fr>
2019-04-25 17:44:36 +0200

    poller: reuse the new settings module for defaults
    Settings in the MDS server were of no help for projects hosting mds.
    
    TODO: We're partly reinventing appconf here, check it out later.

--- mds/provider_poller/settings.py
+++ mds/provider_poller/settings.py
@@ -1,5 +1,10 @@
 from django.conf import settings
 
+from cryptography import fernet
+
+
+POLLER_TOKEN_CACHE = getattr(settings, "POLLER_TOKEN_CACHE", "default")
+POLLER_TOKEN_ENCRYPTION_KEY = fernet.Fernet.generate_key()  # Reset on each restart
 
 # May be set to None in order to pull all provider history
 PROVIDER_POLLER_LIMIT_DAYS = getattr(settings, "PROVIDER_POLLER_LIMIT", 90)
