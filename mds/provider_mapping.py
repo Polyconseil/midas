3067b36ce034938667fac96745d7415ad639723e
AntoineG <antoine.garcia@polyconseil.fr>
2019-08-16 14:05:18 +0200

    Add the old mappings

--- mds/provider_mapping.py
+++ mds/provider_mapping.py
@@ -5,6 +5,7 @@ from mds.enums import (
     PROVIDER_EVENT_TYPE_REASON,
 )
 
+
 # Mappings between Provider and Agency
 #
 # When we poll providers (with provider API) we cast the event reason
@@ -19,83 +20,199 @@ from mds.enums import (
 # https://github.com/CityOfLosAngeles/mobility-data-specification/tree/dev/provider
 PROVIDER_REASON_TO_AGENCY_EVENT = {
     # available #
-    "service_start": (EVENT_TYPE.service_start.name,),
-    "user_drop_off": (EVENT_TYPE.trip_end.name,),
-    "rebalance_drop_off": (EVENT_TYPE.provider_drop_off.name,),
+    PROVIDER_EVENT_TYPE_REASON.service_start.name: (EVENT_TYPE.service_start.name,),
+    PROVIDER_EVENT_TYPE_REASON.user_drop_off.name: (EVENT_TYPE.trip_end.name,),
+    PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name: (
+        EVENT_TYPE.provider_drop_off.name,
+    ),
     # There is no equivalent on the agency side of maintenance_drop_off.
     # This is the closest
-    "maintenance_drop_off": (
+    PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name: (
         EVENT_TYPE.provider_drop_off.name,
         EVENT_TYPE_REASON.maintenance.name,
     ),
-    "agency_drop_off": (PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name,),
+    PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name: (
+        PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name,
+    ),
     # reserved #
-    "user_pick_up": (EVENT_TYPE.trip_start.name,),
+    PROVIDER_EVENT_TYPE_REASON.user_pick_up.name: (EVENT_TYPE.trip_start.name,),
     # unavailable #
-    "maintenance": (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.maintenance.name),
-    "low_battery": (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.low_battery.name),
+    PROVIDER_EVENT_TYPE_REASON.maintenance.name: (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.maintenance.name,
+    ),
+    PROVIDER_EVENT_TYPE_REASON.low_battery.name: (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.low_battery.name,
+    ),
     # removed #
-    "service_end": (EVENT_TYPE.service_end.name,),
+    PROVIDER_EVENT_TYPE_REASON.service_end.name: (EVENT_TYPE.service_end.name,),
     # There is no equivalent on the agency side of rebalance_pick_up.
-    "rebalance_pick_up": (
+    PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name: (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.rebalance.name,
     ),
-    "maintenance_pick_up": (
+    PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name: (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.maintenance.name,
     ),
-    "agency_pick_up": (EVENT_TYPE.city_pick_up.name,),
+    PROVIDER_EVENT_TYPE_REASON.agency_pick_up.name: (EVENT_TYPE.city_pick_up.name,),
+}
+
+OLD_PROVIDER_REASON_TO_AGENCY_EVENT = {
+    PROVIDER_EVENT_TYPE_REASON.service_start.name: EVENT_TYPE.service_start.name,
+    PROVIDER_EVENT_TYPE_REASON.user_drop_off.name: EVENT_TYPE.trip_end.name,
+    PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name:
+        PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,
+    PROVIDER_EVENT_TYPE_REASON.user_pick_up.name: EVENT_TYPE.trip_start.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance.name,
+    PROVIDER_EVENT_TYPE_REASON.low_battery.name:
+        PROVIDER_EVENT_TYPE_REASON.low_battery.name,
+    PROVIDER_EVENT_TYPE_REASON.service_end.name: EVENT_TYPE.service_end.name,
+    PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name:
+        PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name,
+}
+
+OLD_TO_NEW_AGENCY_EVENT = {
+    (EVENT_TYPE.service_start.name,): (EVENT_TYPE.service_start.name,),
+    (EVENT_TYPE.trip_end.name,): (EVENT_TYPE.trip_end.name,),
+    (PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name,): (
+        EVENT_TYPE.provider_drop_off.name,
+    ),
+    (PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,): (
+        EVENT_TYPE.provider_drop_off.name,
+        EVENT_TYPE_REASON.maintenance.name,
+    ),
+    (EVENT_TYPE.trip_start.name,): (EVENT_TYPE.trip_start.name,),
+    (PROVIDER_EVENT_TYPE_REASON.maintenance.name,): (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.maintenance.name,
+    ),
+    (PROVIDER_EVENT_TYPE_REASON.low_battery.name,): (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.low_battery.name,
+    ),
+    (EVENT_TYPE.service_end.name,): (EVENT_TYPE.service_end.name,),
+    (PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name,): (
+        EVENT_TYPE.provider_pick_up.name,
+        EVENT_TYPE_REASON.rebalance.name,
+    ),
+    (PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name,): (
+        EVENT_TYPE.provider_pick_up.name,
+        EVENT_TYPE_REASON.maintenance.name,
+    ),
 }
 
 # Converts an Agency event to a Provider event_type_reason
 # Try to stay consistent with
 # https://github.com/CityOfLosAngeles/mds-core/blob/master/packages/mds-provider/utils.ts
 AGENCY_EVENT_TO_PROVIDER_REASON = {
-    (EVENT_TYPE.register.name,): "service_end",
-    (EVENT_TYPE.service_start.name,): "service_start",
-    (EVENT_TYPE.service_end.name,): "service_end",
-    (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.low_battery.name): "low_battery",
-    (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.maintenance.name): "maintenance",
-    (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.compliance.name): "service_end",
-    (EVENT_TYPE.service_end.name, EVENT_TYPE_REASON.off_hours.name): "service_end",
-    (EVENT_TYPE.provider_drop_off.name,): "rebalance_drop_off",
+    (EVENT_TYPE.register.name,): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (EVENT_TYPE.service_start.name,): PROVIDER_EVENT_TYPE_REASON.service_start.name,
+    (EVENT_TYPE.service_end.name,): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.low_battery.name,
+    ): PROVIDER_EVENT_TYPE_REASON.low_battery.name,
+    (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.maintenance.name,
+    ): PROVIDER_EVENT_TYPE_REASON.maintenance.name,
+    (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.compliance.name,
+    ): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (
+        EVENT_TYPE.service_end.name,
+        EVENT_TYPE_REASON.off_hours.name,
+    ): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (
+        EVENT_TYPE.provider_drop_off.name,
+    ): PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name,
+    # Not in the Agency spec but in the provider spec
     (
         EVENT_TYPE.provider_drop_off.name,
         EVENT_TYPE_REASON.maintenance.name,
-    ): "maintenance_drop_off",  # Not in the Agency spec but in the provider spec
-    (EVENT_TYPE.provider_pick_up.name,): "service_end",
+    ): PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,
+    (EVENT_TYPE.provider_pick_up.name,): PROVIDER_EVENT_TYPE_REASON.service_end.name,
     (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.rebalance.name,
-    ): "rebalance_pick_up",
+    ): PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name,
     (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.maintenance.name,
-    ): "maintenance_pick_up",
+    ): PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name,
     (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.charge.name,
-    ): "maintenance_pick_up",
+    ): PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name,
     (
         EVENT_TYPE.provider_pick_up.name,
         EVENT_TYPE_REASON.compliance.name,
-    ): "service_end",
+    ): PROVIDER_EVENT_TYPE_REASON.service_end.name,
     # no city_pick_up in provider's spec
-    (EVENT_TYPE.city_pick_up.name,): "agency_pick_up",
-    (EVENT_TYPE.reserve.name,): "user_pick_up",
-    (EVENT_TYPE.cancel_reservation.name,): "user_drop_off",
-    (EVENT_TYPE.trip_start.name,): "user_pick_up",
-    (EVENT_TYPE.trip_enter.name,): "user_pick_up",
-    (EVENT_TYPE.trip_leave.name,): "service_end",
-    (EVENT_TYPE.trip_end.name,): "user_drop_off",
-    (EVENT_TYPE.deregister.name,): "service_end",
-    (EVENT_TYPE.deregister.name, EVENT_TYPE_REASON.missing.name): "service_end",
-    (EVENT_TYPE.deregister.name, EVENT_TYPE_REASON.decommissioned.name): "service_end",
+    (EVENT_TYPE.city_pick_up.name,): PROVIDER_EVENT_TYPE_REASON.agency_pick_up.name,
+    (EVENT_TYPE.reserve.name,): PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    (
+        EVENT_TYPE.cancel_reservation.name,
+    ): PROVIDER_EVENT_TYPE_REASON.user_drop_off.name,
+    (EVENT_TYPE.trip_start.name,): PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    (EVENT_TYPE.trip_enter.name,): PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    (EVENT_TYPE.trip_leave.name,): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (EVENT_TYPE.trip_end.name,): PROVIDER_EVENT_TYPE_REASON.user_drop_off.name,
+    (EVENT_TYPE.deregister.name,): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (
+        EVENT_TYPE.deregister.name,
+        EVENT_TYPE_REASON.missing.name,
+    ): PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    (
+        EVENT_TYPE.deregister.name,
+        EVENT_TYPE_REASON.decommissioned.name,
+    ): PROVIDER_EVENT_TYPE_REASON.service_end.name,
     # Not in the MDS agency spec: probably not used
-    (PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name,): "agency_drop_off",  # not used
-    (PROVIDER_EVENT_TYPE_REASON.agency_pick_up.name,): "agency_pick_up",  # not used
-    (EVENT_TYPE.battery_charged.name,): "maintenance_drop_off",  # not used
+    (
+        PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name,
+    ): PROVIDER_EVENT_TYPE_REASON.agency_drop_off.name,  # not used
+    (
+        PROVIDER_EVENT_TYPE_REASON.agency_pick_up.name,
+    ): PROVIDER_EVENT_TYPE_REASON.agency_pick_up.name,  # not used
+    (
+        EVENT_TYPE.battery_charged.name,
+    ): PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,  # not used
+}
+
+OLD_AGENCY_EVENT_TO_PROVIDER_REASON = {
+    EVENT_TYPE.service_start.name: PROVIDER_EVENT_TYPE_REASON.service_start.name,
+    EVENT_TYPE.cancel_reservation.name: PROVIDER_EVENT_TYPE_REASON.user_drop_off.name,
+    EVENT_TYPE.trip_end.name: PROVIDER_EVENT_TYPE_REASON.user_drop_off.name,
+    PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name:
+        PROVIDER_EVENT_TYPE_REASON.rebalance_drop_off.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,
+    EVENT_TYPE.battery_charged.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance_drop_off.name,
+    EVENT_TYPE.reserve.name: PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    EVENT_TYPE.trip_start.name: PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    EVENT_TYPE.trip_enter.name: PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,
+    EVENT_TYPE.trip_leave.name:
+        PROVIDER_EVENT_TYPE_REASON.user_pick_up.name,  # This is on really bad...
+    PROVIDER_EVENT_TYPE_REASON.low_battery.name:
+        PROVIDER_EVENT_TYPE_REASON.low_battery.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance.name,
+    EVENT_TYPE.deregister.name: PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    EVENT_TYPE.service_end.name: PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    EVENT_TYPE.register.name: PROVIDER_EVENT_TYPE_REASON.service_end.name,
+    PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name:
+        PROVIDER_EVENT_TYPE_REASON.rebalance_pick_up.name,
+    PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name:
+        PROVIDER_EVENT_TYPE_REASON.maintenance_pick_up.name,
 }
 
 # Inside the Provider API, maps the event_type_reason to the corresponding event_type
--- tests/test_provider_mapping.py
+++ tests/test_provider_mapping.py
@@ -1,20 +1,67 @@
 from mds.provider_mapping import (
-    PROVIDER_REASON_TO_AGENCY_EVENT,
     AGENCY_EVENT_TO_PROVIDER_REASON,
+    OLD_AGENCY_EVENT_TO_PROVIDER_REASON,
+    OLD_PROVIDER_REASON_TO_AGENCY_EVENT,
+    OLD_TO_NEW_AGENCY_EVENT,
+    PROVIDER_REASON_TO_AGENCY_EVENT,
 )
+
+# This file tests the different functions of this old and new mapping:
+# Old:
+# Provider    --old_p_a-->    Agency    --old_a_p-->    Provider
+# New:                   --old_to_new_a--
+# Provider    --new_p_a-->    Agency    --new_a_p-->    Provider
 
 
 def test_provider_agency_mapping():
     """
-    Checks that the bidirectional mappings are kind of consistent
+    Checks that the bidirectional mappings are kind of consistent.
+    new_a_p ○ new_p_a = id
     """
     for provider_reason, agency_event in PROVIDER_REASON_TO_AGENCY_EVENT.items():
         assert AGENCY_EVENT_TO_PROVIDER_REASON[agency_event] == provider_reason
+
+
+def test_old_provider_agency_mapping():
+    """
+    Checks that the old bidirectional mappings are kind of consistent
+    old_a_p ○ old_p_a = id
+    """
+    for provider_reason, agency_event in OLD_PROVIDER_REASON_TO_AGENCY_EVENT.items():
+        assert OLD_AGENCY_EVENT_TO_PROVIDER_REASON[agency_event] == provider_reason
+
+
+def test_old_to_new_is_same_as_new():
+    """
+    Checks that when we apply the migration to the old events,
+    we have the same result as with the new mappings.
+    old_to_new_a ○ old_p_a = new_p_a
+    """
+    for reason, old_agency_event in OLD_PROVIDER_REASON_TO_AGENCY_EVENT.items():
+        agency_event = OLD_TO_NEW_AGENCY_EVENT[(old_agency_event,)]
+        new_agency_event = PROVIDER_REASON_TO_AGENCY_EVENT[reason]
+        assert agency_event == new_agency_event
+
+
+def test_migration_new_idem():
+    """
+    Checks that when we apply the old_to_new migration to the new events,
+    we don't change anything.
+    old_to_new_a(new)= new
+    """
+    for _, agency_event in PROVIDER_REASON_TO_AGENCY_EVENT.items():
+        new_agency_event = OLD_TO_NEW_AGENCY_EVENT.get(agency_event, agency_event)
+        assert agency_event == new_agency_event
 
 
 def test_migration_idempotent():
     """
-    Checks that when we apply the migration a second time, we don't change the event.
+    Checks that applying the old to new mapping twice is the same as applying it once
+    old_to_new_a ○ old_to_new_a = old_to_new_a
     """
-    for provider_reason, agency_event in PROVIDER_REASON_TO_AGENCY_EVENT.items():
-        assert AGENCY_EVENT_TO_PROVIDER_REASON[agency_event] == provider_reason
+    for _, agency_event in OLD_PROVIDER_REASON_TO_AGENCY_EVENT.items():
+        new_agency_event = OLD_TO_NEW_AGENCY_EVENT.get(agency_event, agency_event)
+        new_new_agency_event = OLD_TO_NEW_AGENCY_EVENT.get(
+            new_agency_event, new_agency_event
+        )
+        assert new_agency_event == new_new_agency_event
