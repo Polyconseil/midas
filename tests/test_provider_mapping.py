d31bb960a64d857465ecaf897a6132e4a19ca68f
AntoineG <antoine.garcia@polyconseil.fr>
2019-08-16 14:02:32 +0200

    Add test for the migration

--- tests/test_provider_mapping.py
+++ tests/test_provider_mapping.py
@@ -10,3 +10,11 @@ def test_provider_agency_mapping():
     """
     for provider_reason, agency_event in PROVIDER_REASON_TO_AGENCY_EVENT.items():
         assert AGENCY_EVENT_TO_PROVIDER_REASON[agency_event] == provider_reason
+
+
+def test_migration_idempotent():
+    """
+    Checks that when we apply the migration a second time, we don't change the event.
+    """
+    for provider_reason, agency_event in PROVIDER_REASON_TO_AGENCY_EVENT.items():
+        assert AGENCY_EVENT_TO_PROVIDER_REASON[agency_event] == provider_reason
